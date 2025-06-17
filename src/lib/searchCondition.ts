ced48544b95921ca190f1a34a76697ddd66ac3bd
jbyoum <jbyoum3@gmail.com>
2025-06-17 15:11:58 +0900

    #1 공통 일부

---
+++ src/lib/searchCondition.ts
@@ -0,0 +1,68 @@
+import { PageParamsType } from "../structs/commonStructs";
+import { prisma } from "./prisma";
+
+export async function buildSearchCondition(
+  params: PageParamsType,
+  args?: Record<string, any>
+): Promise<{
+  whereCondition: Record<string, { contains: string }> | {};
+  pageCondition: { skip: number; take: number };
+  bothCondition: {
+    skip: number;
+    take: number;
+    where: Record<string, { contains: string }> | {};
+  };
+}> {
+  const pageCondition = {
+    skip: (params.page - 1) * params.limit,
+    take: params.limit,
+  };
+
+  let whereCondition: Record<string, { contains: string }> | {} = {};
+
+  if (args && "userId" in args && "role" in args) {
+    if (args.role === "ADMIN") {
+      whereCondition = {
+        article: {
+          user: {
+            id: args.userId,
+          },
+        },
+      };
+    } else if (args.role === "USER") {
+      const adminId = await prisma.userInfo.findFirst({
+        where: {
+          userId: args.userId,
+        },
+        include: {
+          apartmentInfo: {
+            include: {
+              user: {
+                select: {
+                  id: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      whereCondition = {
+        article: {
+          user: {
+            id: adminId,
+          },
+        },
+      };
+    }
+  }
+  const bothCondition = {
+    ...pageCondition,
+    where: whereCondition,
+  };
+
+  return {
+    whereCondition,
+    pageCondition,
+    bothCondition,
+  };
+}
