465186db3c1a8523e808bd72d1790e0dd060ddbc
leeminho <twomean99@gmail.com>
2025-06-18 14:08:48 +0900

    struct수정

--- src/structs/residentStruct.ts
+++ src/structs/residentStruct.ts
@@ -1,24 +1,35 @@
-import { object, string, number, enums, optional, boolean } from "superstruct";
+import {
+  object,
+  string,
+  number,
+  enums,
+  optional,
+  boolean,
+  size,
+  pattern,
+} from "superstruct";
 
 const RESIDENCE_STATUS = ["RESIDENCE", "NO_RESIDENCE"] as const;
 const HOUSEHOLDER_STATUS = ["HOUSEHOLDER", "MEMBER"] as const;
 const APPROVAL_STATUS = ["UNRECEIVED", "PENDING", "APPROVED"] as const;
 
+const phoneRegex = /^010-\d{4}-\d{4}$/;
+
 export const createResidentBodyStruct = object({
   building: number(),
   unitNumber: number(),
-  contact: string(),
-  name: string(),
-  email: optional(string()),
+  contact: pattern(size(string(), 10, 20), phoneRegex),
+  name: size(string(), 50),
+  email: optional(size(string(), 5, 320)),
   isHouseholder: enums(["HOUSEHOLDER", "MEMBER"]),
 });
 
 export const UpdateResidentBodyStruct = object({
   building: optional(number()),
   unitNumber: optional(number()),
-  contact: optional(string()),
-  name: optional(string()),
-  email: optional(string()),
+  contact: pattern(size(string(), 10, 20), phoneRegex),
+  name: size(string(), 50),
+  email: optional(size(string(), 5, 320)),
   residenceStatus: optional(enums(RESIDENCE_STATUS)),
   isHouseholder: optional(enums(HOUSEHOLDER_STATUS)),
   isRegistered: optional(boolean()),
