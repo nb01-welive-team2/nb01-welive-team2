70a3dc43ddf1af1f8d4e04a62ba3a2d17fff651c
Zen-nojen <jiyoyo1234@naver.com>
2025-06-19 10:36:03 +0900

    [#3] express id, role 설정

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,13 @@
 import Express from "express";
+import { USER_ROLE } from "@prisma/client";
 
 declare global {
   namespace Express {
     interface Request {
       user?: {
         userId: number;
+        id: string;
+        role: USER_ROLE;
       };
     }
   }
