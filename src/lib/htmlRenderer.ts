1ef920b3073e2ab08349f5d60f6c8eb1efc61180
jbyoum <jbyoum3@gmail.com>
2025-07-24 15:51:11 +0900

    임시

--- src/lib/htmlRenderer.ts
+++ src/lib/htmlRenderer.ts
@@ -5,7 +5,7 @@ import { PROTOCOL, SERVER_URL, REDIRECT_PORT } from "./constance";
 export function renderHtmlWithUrl(filename: string): string {
   const filePath = path.join(process.cwd(), "public/uploads", filename);
   let html = fs.readFileSync(filePath, "utf-8");
-  const baseUrl = `${PROTOCOL}://${SERVER_URL}:${REDIRECT_PORT}`;
+  const baseUrl = `${SERVER_URL}:${REDIRECT_PORT}`;
   html = html.replace(/__SERVER_URL__/g, baseUrl);
   return html;
 }
